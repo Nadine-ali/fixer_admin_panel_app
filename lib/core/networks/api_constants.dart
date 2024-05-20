0aba1b05b8c87c8139d1c11c98bce89d3baabfe2
MagdKamaldev <magddevf@gmail.com>
2024-05-20 03:40:51 +0300

    refactoring ip

--- lib/core/networks/api_constants.dart
+++ lib/core/networks/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  static const String baseUrl = "https://192.168.1.7:500/";
+  static const String baseUrl = "http://192.168.1.7:5000/";
   static const String login = "login";
 }
