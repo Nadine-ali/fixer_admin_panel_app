0e9b7eb50b7df8bdef4676e14ed8a6d7553e58c5
Nadine-ali <nadinealiselim@gmail.com>
2024-05-27 18:38:11 +0300

    admin ui edit is done

--- lib/features/admin/presentation/view/widget/Admin_Card_model.dart
+++ lib/features/admin/presentation/view/widget/Admin_Card_model.dart
@@ -4,6 +4,7 @@ import 'package:fixer_admin_panel_app/core/themes/text_styles.dart';
 import 'package:fixer_admin_panel_app/features/admin/data/models/admin_model.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AdminCardModel extends StatelessWidget {
   final AdminModel admin;
@@ -21,46 +22,42 @@ class AdminCardModel extends StatelessWidget {
     return InkWell(
       onTap: toggleProfileView,
       child: Container(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(15),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(8),
           color: ColorManager.white,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.person,
-                color: Colors.amber,
-                size: 40,
-              ),
-              horizontalSpace(5),
-              Column(
-                children: [
-                  Text(
-                    admin.name!,
-                    style:
-                        TextStyles.normal.copyWith(fontWeight: FontWeight.w400),
-                  ),
-                  verticalSpace(10),
-                  Text(admin.role!,
-                      style: TextStyles.normal
-                          .copyWith(fontWeight: FontWeight.w500))
-                ],
-              ),
-              horizontalSpace(10),
-              InkWell(
-                  onTap: toggleEditForm,
-                  child: const Icon(
-                    Icons.edit_outlined,
-                    color: ColorManager.black,
-                    size: 25,
-                  ))
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.person,
+              color: ColorManager.grey,
+              size: 60,
+            ),
+            verticalSpace(30),
+            Column(
+              children: [
+                Text(
+                  admin.name!,
+                  style:
+                      TextStyles.normal.copyWith(fontWeight: FontWeight.w700),
+                ),
+                verticalSpace(10),
+                Text(admin.role!,
+                    style: TextStyles.normal
+                        .copyWith(fontWeight: FontWeight.w500))
+              ],
+            ),
+            verticalSpace(35),
+            InkWell(
+                onTap: toggleEditForm,
+                child: const Icon(
+                  Icons.edit_outlined,
+                  color: ColorManager.black,
+                  size: 25,
+                ))
+          ],
         ),
       ),
     );
--- lib/features/admin/presentation/view/widget/admin_body.dart
+++ lib/features/admin/presentation/view/widget/admin_body.dart
@@ -9,6 +9,7 @@ import 'package:fixer_admin_panel_app/core/helpers/spacing.dart';
 import 'package:fixer_admin_panel_app/core/themes/text_styles.dart';
 import 'package:fixer_admin_panel_app/features/admin/data/models/admin_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AdminBody extends StatefulWidget {
   const AdminBody({super.key});
@@ -62,55 +63,70 @@ class _AdminBodyState extends State<AdminBody> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: InkWell(
-                  onTap: _toggleEditEntryForm,
-                  child: Container(
-                    width: 120,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: ColorManager.primary,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Add user",
-                        style: TextStyles.normal
-                            .copyWith(color: ColorManager.white),
+              Text("All Admins",style:TextStyles.headings,),
+              verticalSpace(50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  BlocBuilder<AdminCubit, AdminState>(
+                    builder: (context, state) {
+                      AdminCubit cubit = AdminCubit.get(context);
+                      return SizedBox(
+                        width: 100.w,
+                        height: 425.h,
+                        // height: size.height * 0.7,
+                        // width: double.infinity,
+                        child: GridView.count(
+                          childAspectRatio: 174/195,
+                          // childAspectRatio: 12/4,
+                          crossAxisCount: size.width > 1300 ? 1 : 1,
+                          // crossAxisCount: size.width > 1300 ? 3 : 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          children: cubit.admins.isNotEmpty
+                              ? cubit.admins.map((admin) {
+                                  return AdminCardModel(
+                                    admin: admin,
+                                    toggleEditForm: _toggleEditEntryForm,
+                                    toggleProfileView: () =>
+                                        _toggleProfileView(admin),
+                                  );
+                                }).toList()
+                              : List.generate(
+                                  6,
+                                  (index) => const ShimmerAdminCard(),
+                                ),
+                        ),
+                      );
+                    },
+                  ),
+                  horizontalSpace(10),
+                   InkWell(
+                    onTap: _toggleEditEntryForm,
+                    child: Container(
+                      width: 100.w,
+                      height: 425.h,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: ColorManager.primary),
+                        borderRadius: BorderRadius.circular(8),
+                        color: ColorManager.white,
+                      ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.add,size: 40,color: ColorManager.primary,),
+                            Text(
+                              "Add Admin",
+                              style: TextStyles.normal
+                                  .copyWith(color: ColorManager.primary),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              verticalSpace(50),
-              BlocBuilder<AdminCubit, AdminState>(
-                builder: (context, state) {
-                  AdminCubit cubit = AdminCubit.get(context);
-                  return SizedBox(
-                    height: size.height * 0.7,
-                    width: double.infinity,
-                    child: GridView.count(
-                      childAspectRatio: 12 / 4,
-                      crossAxisCount: size.width > 1300 ? 3 : 2,
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      children: cubit.admins.isNotEmpty
-                          ? cubit.admins.map((admin) {
-                              return AdminCardModel(
-                                admin: admin,
-                                toggleEditForm: _toggleEditEntryForm,
-                                toggleProfileView: () =>
-                                    _toggleProfileView(admin),
-                              );
-                            }).toList()
-                          : List.generate(
-                              6,
-                              (index) => const ShimmerAdminCard(),
-                            ),
-                    ),
-                  );
-                },
+                ],
               ),
             ],
           ),
